2ad1ebc3b97c89cb29db05cfca23ab53fdd6179d
shirishaardak <50703497+shirishaardak@users.noreply.github.com>
2023-06-22 20:40:16 +0530

    update code
    update code

--- backtesting_strategy/backtesting_strategy/Tranding_finding.py
+++ backtesting_strategy/backtesting_strategy/Tranding_finding.py
@@ -7,7 +7,7 @@ from plotly.subplots import make_subplots
 from scipy.signal import argrelextrema
 import plotly.graph_objects as go 
 import yfinance as yf
-from utility.backtesting import Backtest
+
 import pandas_ta as ta
 import streamlit as st  
 from streamlit_autorefresh import st_autorefresh
@@ -23,11 +23,16 @@ df = ta.ha(open_=df.Open, high=df.High, low=df.Low, close=df.Close,)
 df['RSI'] = ta.rsi(close=df.HA_close, length=14)
 df['ATR'] = ta.atr(high=df.HA_high, low=df.HA_low, close=df.HA_close, length=14)
 df['ATR-20'] = ta.sma(close=df['ATR'], length=20)
-max_idx = argrelextrema(df['HA_close'].values, np.greater_equal, order=21)[0]
-min_idx = argrelextrema(df['HA_close'].values, np.less_equal, order=21)[0]
+max_idx = argrelextrema(df['HA_close'].values, np.greater_equal, order=10)[0]
+min_idx = argrelextrema(df['HA_close'].values, np.less_equal, order=10)[0]
 # max_idx = max_idx[:-1]
 # min_idx = min_idx[:-1]
 
+high_low= np.concatenate((max_idx, min_idx))
+high_low_up = np.sort(high_low)
+high_low_up = high_low_up[:-1]
+
+df['max_high_low'] = df.iloc[high_low_up]['HA_close']
 df['max_high'] = df.iloc[max_idx]['HA_close']
 df['max_low'] = df.iloc[min_idx]['HA_close']
 
@@ -36,12 +41,9 @@ trand = df['HA_close'].iloc[0]
 
 
 for i in range(len(df)):
-    if df['HA_close'][i] == df['max_high'][i]:
-        trand = df['HA_high'][i-1]
-        df['trand-line'][i] = df['HA_high'][i-1]
-    elif df['HA_close'][i] == df['max_low'][i]:
-        trand = df['HA_low'][i-1]
-        df['trand-line'][i] = df['HA_low'][i-1]    
+    if df['HA_close'][i] == df['max_high_low'][i]:
+        trand = df['HA_close'][i-1]
+        df['trand-line'][i] = df['HA_close'][i-1]      
     else:
         df['trand-line'][i] = trand
 
@@ -54,9 +56,9 @@ fig.add_trace(go.Candlestick(x=df.index,
                 high=df['HA_high'],
                 low=df['HA_low'],
                 close=df['HA_close']), row=1, col=1)
-fig.update_layout(title_text="title", margin={"r": 0, "t": 0, "l": 0, "b": 0}, height=1200, width=1750)
-fig.add_trace(go.Scatter(mode='markers', x=df.iloc[max_idx].index, y=df.iloc[max_idx]['HA_high'], marker=dict(color='Blue',size=12,), name="Buy"), row=1, col=1)
-fig.add_trace(go.Scatter(mode='markers', x=df.iloc[min_idx].index, y=df.iloc[min_idx]['HA_low'], marker=dict(color='Red',size=12,), name="Sell"), row=1, col=1)
+fig.update_layout(title_text="title", margin={"r": 0, "t": 0, "l": 0, "b": 0}, height=1500, width=1200)
+fig.add_trace(go.Scatter(mode='markers', x=df.iloc[high_low_up].index, y=df.iloc[high_low_up]['HA_close'], marker=dict(color='Blue',size=12,), name="Buy"), row=1, col=1)
+#fig.add_trace(go.Scatter(mode='markers', x=df.iloc[min_idx].index, y=df.iloc[min_idx]['HA_low'], marker=dict(color='Red',size=12,), name="Sell"), row=1, col=1)
 fig.add_trace(go.Scatter(x=df.index, y=df['trand-line']))
 fig.add_trace(go.Scatter(x=df.index, y=df['RSI']), row=2, col=1)
 fig.add_trace(go.Scatter(x=df.index, y=df['ATR']), row=3, col=1)
